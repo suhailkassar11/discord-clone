75e7b18994a2bd3d831dbaf287f14ef293673b18
suhailKassar11 <suhail.1923co1011@kiet.edu>
2024-01-08 22:01:22 +0530

    authentication done

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({children}:{children:React.ReactNode}) => {
     return ( 
-        <div className="bg-red-500 h-full ">
+        <div className="h-full flex justify-center items-center">
             {children}
         </div>
      );
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p>hello dicord clone</p>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+      <ModeToggle/>
+    </div>
   )
 }
